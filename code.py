c142b377f9ed44c64b08527332ce7dd1b23982de
njacklin <njacklin@gmail.com>
2022-03-17 08:41:38 -0700

    initial 3 groups with some text working

--- code.py
+++ code.py
@@ -7,6 +7,7 @@ import time
 
 from adafruit_bitmap_font import bitmap_font
 from adafruit_display_text import label
+from adafruit_display_shapes.rect import Rect
 
 # SETUP ----------------------------------------------------------------------
 
@@ -30,44 +31,87 @@ DGROUP_2021DAY11 = 2
 
 # build disp_group[DGROUP_MAIN] --------------------------
 
+# background
+bg = Rect(0, 0, board.DISPLAY.width, board.DISPLAY.height, fill=0x0f0f23)
+disp_group[DGROUP_MAIN].append(bg)
+
 # AOC label at top
-label_aoc = label.Label(font, text="Advent of Code\n  int y=2021;", color=0x009900)
+label_aoc = label.Label(font, text="Advent of Code\n   int y=2021;", color=0x009900)
 label_aoc.anchor_point = (0.0,0.0) # upper left
 label_aoc.anchored_position = (0,0)
 disp_group[DGROUP_MAIN].append(label_aoc)
 
 # "VTS Leaderboard"
-label_leaderboard = label.Label(font,text=" VTS Leaderboard:")
-label_leaderboard.anchor_point = (0.0,0.0) # upper left
-label_leaderboard.anchored_position = (0,30)
+label_leaderboard = label.Label(font,text="~ VTS Leaderboard ~")
+label_leaderboard.anchor_point = (0.5,0.0) # middle top
+label_leaderboard.anchored_position = (board.DISPLAY.width/2,45)
 disp_group[DGROUP_MAIN].append(label_leaderboard)
+
+# first place
+label_first = label.Label(font,text="1st:38* John Moon",scale=1)
+label_first.anchor_point = (0.0,0.0) # left top
+label_first.anchored_position = (0,60)
+disp_group[DGROUP_MAIN].append(label_first)
+
+# second place
+label_2nd = label.Label(font,text="2nd:36* Neil Jacklin")
+label_2nd.anchor_point = (0.0,0.0) # left top
+label_2nd.anchored_position = (0,75)
+disp_group[DGROUP_MAIN].append(label_2nd)
+
+# third place
+label_3rd = label.Label(font,text="3rd:31*DaveBuscaglia")
+label_3rd.anchor_point = (0.0,0.0) # left top
+label_3rd.anchored_position = (0,90)
+disp_group[DGROUP_MAIN].append(label_3rd)
 
 # build disp_group[DGROUP_50STARS] -----------------------
 
+# background
+bg = Rect(0, 0, board.DISPLAY.width, board.DISPLAY.height, fill=0x0f0f23)
+disp_group[DGROUP_50STARS].append(bg)
+
 # AOC label at top
-label_aoc = label.Label(font, text="Advent of Code\n  int y=2021;", color=0x009900)
-label_aoc.anchor_point = (0.0,0.0) # upper left
+label_aoc = label.Label(font, text="Advent of Code\n   int y=2021;", color=0x009900)
+label_aoc.anchor_point = (0.0,0.0) # left top
 label_aoc.anchored_position = (0,0)
 disp_group[DGROUP_50STARS].append(label_aoc)
 
 # "First to 50*"
-label_firstto50 = label.Label(font,text=" First to 50*:")
-label_firstto50.anchor_point = (0.0,0.0) # upper left
-label_firstto50.anchored_position = (0,30)
+label_firstto50 = label.Label(font,text="First to 50*")
+label_firstto50.anchor_point = (0.5,0.0) # middle top
+label_firstto50.anchored_position = (board.DISPLAY.width/2,45)
 disp_group[DGROUP_50STARS].append(label_firstto50)
+
+# Name
+label_firstto50name = label.Label(font,text="Dave Buscaglia")
+label_firstto50name.anchor_point = (0.5,0.0) # middle top
+label_firstto50name.anchored_position = (board.DISPLAY.width/2,60)
+disp_group[DGROUP_50STARS].append(label_firstto50name)
+
+# Date
+label_firstto50date = label.Label(font,text="7 MAR 2022")
+label_firstto50date.anchor_point = (0.5,0.0) # middle top
+label_firstto50date.anchored_position = (board.DISPLAY.width/2,75)
+disp_group[DGROUP_50STARS].append(label_firstto50date)
+
 
 # build disp_group[DGROUP_2021DAY11] ---------------------
 
+# background
+bg = Rect(0, 0, board.DISPLAY.width, board.DISPLAY.height, fill=0x0f0f23)
+disp_group[DGROUP_2021DAY11].append(bg)
+
 # AOC label at top
-label_aoc = label.Label(font, text="Advent of Code\n  int y=2021;", color=0x009900)
-label_aoc.anchor_point = (0.0,0.0) # upper left
+label_aoc = label.Label(font, text="Advent of Code\n   int y=2021;", color=0x009900)
+label_aoc.anchor_point = (0.0,0.0) # left top
 label_aoc.anchored_position = (0,0)
 disp_group[DGROUP_2021DAY11].append(label_aoc)
 
 # "Day 11 Demo"
-label_demo = label.Label(font,text="2021 Day 11 Demo:")
-label_demo.anchor_point = (0.0,0.0) # upper left
-label_demo.anchored_position = (0,30)
+label_demo = label.Label(font,text="2021 DAY 11")
+label_demo.anchor_point = (0.5,1.0) # middle bottom
+label_demo.anchored_position = (board.DISPLAY.width/2,board.DISPLAY.height)
 disp_group[DGROUP_2021DAY11].append(label_demo)
 
 # init display
